715d526b59d425ff6b0574c41924aa763d76a198
Justin Beaulieu <beaulieudev@gmail.com>
2019-02-12 01:30:56 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,4 +1,4 @@
-class CreateCharecters < ActiveRecord::Migration[4.2]
+class CreateCharacters < ActiveRecord::Migration[4.2]
   def change
     create_table :charecters do |t|
       #primary key of :id is created for us!
