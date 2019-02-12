3eca493fb88586ebb409919f1fa27e17e1c1fdef
Justin Beaulieu <beaulieudev@gmail.com>
2019-02-12 01:13:38 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,8 +1,9 @@
 class CreateActors < ActiveRecord::Migration
   def change
-    create_table :shows do |t|
+    create_table :actors do |t|
       #primary key of :id is created for us!
-      t.string :name
+      t.string :first_name
+      t.string :last_name
     end
   end
 end
